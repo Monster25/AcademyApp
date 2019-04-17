81b9d3d8f603390f1606c87be2ff3091e6a94e1f
Andrei Cioanca <andreicioanca25@gmail.com>
2019-04-17 03:32:09 +0200

    split activity into fragments. YEET <3

---
+++ app/src/main/java/com/application/academy/AgendaFragment.java
@@ -0,0 +1,115 @@
+package com.application.academy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+public class AgendaFragment extends Fragment {
+    Button sendButton, removeButton, setButton;
+    EditText studentName, sessionNumbers, studentId, studentName2, sessionNumbers2;
+    CheckBox paid, paid2;
+    TextView example, example2, example3;
+
+    private StudentList studentList;
+    private int lastId = -1;
+    private StudentList students;
+    private FirebaseAdapter adapter;
+    private StudentViewModel viewModel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.agenda_fragment, parent, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        //Interactable UI assignments
+        studentName = view.findViewById(R.id.nameField);
+        sessionNumbers = view.findViewById(R.id.sessionNumbers);
+        paid = view.findViewById(R.id.paid);
+        sendButton = view.findViewById(R.id.addButton);
+        example = view.findViewById(R.id.example);
+        example2 = view.findViewById(R.id.example2);
+        example3 = view.findViewById(R.id.example3);
+        removeButton = view.findViewById(R.id.removeButton);
+        studentId = view.findViewById(R.id.studentId);
+        setButton = view.findViewById(R.id.setButton);
+        studentName2 = view.findViewById(R.id.nameField2);
+        sessionNumbers2 = view.findViewById(R.id.sessionNumbers2);
+        paid2 = view.findViewById(R.id.paid2);
+
+
+        viewModel = ViewModelProviders.of(this).get(StudentViewModel.class);
+
+        LiveData<Student> studentLiveData = viewModel.getStudentLiveData();
+        adapter = FirebaseAdapter.getInstance();
+
+        studentLiveData.observe(this, new Observer<Student>()
+        {
+            public void onChanged(@Nullable Student student)
+            {
+                if (student!=null)
+                {
+                    studentList = viewModel.getStudentList();
+                    lastId = studentList.getLastStudent().getId();
+
+                    example.setText(studentList.getStudent(0).getName());
+                    // example.setText(studentList.getStudent(0).getName());
+                    // example2.setText(studentList.getStudent(1).getName());
+                    // example3.setText(studentList.getStudent(2).getName());
+                    // example.setText(dataSnapshot.child("Student1").getValue(Student.class).getName());
+                }
+            }
+        });
+
+        //add student
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Student student = new Student(studentName.getText().toString(), paid.isChecked(), Integer.parseInt(sessionNumbers.getText().toString()), lastId+1);
+                viewModel.addStudent(studentName.getText().toString(), paid.isChecked(), Integer.parseInt(sessionNumbers.getText().toString()), lastId+1);
+                //myRef.child("Student"+student.getId()).setValue(student);
+                studentName.setText("");
+                paid.setChecked(false);
+                sessionNumbers.setText("");
+            }
+        });
+
+
+        //Remove student
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.removeStudent("Student"+studentId.getText().toString());
+                // myRef.child("Student"+studentId.getText()).removeValue();
+                studentId.setText("");
+            }
+        });
+
+        //Set Student
+        setButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //int id = Integer.parseInt(studentId.getText().toString());
+                viewModel.setStudent(studentName2.getText().toString(), paid2.isChecked(), Integer.parseInt(sessionNumbers2.getText().toString()), Integer.parseInt(studentId.getText().toString()));
+                //Student student = new Student(studentName2.getText().toString(), paid2.isChecked(), Integer.parseInt(sessionNumbers2.getText().toString()), id);
+                // myRef.child("Student"+studentId.getText()).setValue(student);
+                studentId.setText("");
+                studentName2.setText("");
+                paid2.setChecked(false);
+                sessionNumbers2.setText("");
+            }
+        });
+    }
+}
--- app/src/main/java/com/application/academy/MainActivity.java
+++ app/src/main/java/com/application/academy/MainActivity.java
@@ -2,20 +2,25 @@ package com.application.academy;
 
 import android.os.Bundle;
 //import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -23,10 +28,9 @@ import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button sendButton, removeButton, setButton;
-    EditText studentName, sessionNumbers, studentId, studentName2, sessionNumbers2;
-    CheckBox paid, paid2;
-    TextView example, example2, example3;
+
+    BottomNavigationView bottomNav;
+
     //Exclude actual incrementer from database
     @Exclude
     public final static AtomicInteger incrementer = new AtomicInteger(-1); //id incrementer
@@ -41,92 +45,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Interactable UI assignments
-        studentName = findViewById(R.id.nameField);
-        sessionNumbers = findViewById(R.id.sessionNumbers);
-        paid = findViewById(R.id.paid);
-        sendButton = findViewById(R.id.addButton);
-        example = findViewById(R.id.example);
-        example2 = findViewById(R.id.example2);
-        example3 = findViewById(R.id.example3);
-        removeButton = findViewById(R.id.removeButton);
-        studentId = findViewById(R.id.studentId);
-        setButton = findViewById(R.id.setButton);
-        studentName2 = findViewById(R.id.nameField2);
-        sessionNumbers2 = findViewById(R.id.sessionNumbers2);
-        paid2 = findViewById(R.id.paid2);
 
-        viewModel = ViewModelProviders.of(this).get(StudentViewModel.class);
+        bottomNav = findViewById(R.id.bottom_navigation);
 
-        LiveData<Student> studentLiveData = viewModel.getStudentLiveData();
-        adapter = FirebaseAdapter.getInstance();
-
-        studentLiveData.observe(this, new Observer<Student>()
-        {
-            public void onChanged(@Nullable Student student)
+        //Bottom navigation listeners
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
             {
-                if (student!=null)
-                {
-                    studentList = viewModel.getStudentList();
-                    lastId = studentList.getLastStudent().getId();
-
-                    example.setText(studentList.getStudent(0).getName());
-                  // example.setText(studentList.getStudent(0).getName());
-                   // example2.setText(studentList.getStudent(1).getName());
-                   // example3.setText(studentList.getStudent(2).getName());
-                   // example.setText(dataSnapshot.child("Student1").getValue(Student.class).getName());
-                }
+                displayView(item.getItemId());
+                return true;
             }
         });
+    displayView(R.id.menu_agenda);
 
-        //Writing to the database v
-
-        // FirebaseDatabase database = FirebaseDatabase.getInstance();
-         //final DatabaseReference myRef = database.getReference("Students");
-
-        //Add Student
-         sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Student student = new Student(studentName.getText().toString(), paid.isChecked(), Integer.parseInt(sessionNumbers.getText().toString()), lastId+1);
-                viewModel.addStudent(studentName.getText().toString(), paid.isChecked(), Integer.parseInt(sessionNumbers.getText().toString()), lastId+1);
-                //myRef.child("Student"+student.getId()).setValue(student);
-                studentName.setText("");
-                paid.setChecked(false);
-                sessionNumbers.setText("");
-            }
-        });
-
-
-         //Remove student
-         removeButton.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 viewModel.removeStudent("Student"+studentId.getText().toString());
-               // myRef.child("Student"+studentId.getText()).removeValue();
-                studentId.setText("");
-             }
-        });
-
-         //Set Student
-        setButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //int id = Integer.parseInt(studentId.getText().toString());
-                viewModel.setStudent(studentName2.getText().toString(), paid2.isChecked(), Integer.parseInt(sessionNumbers2.getText().toString()), Integer.parseInt(studentId.getText().toString()));
-                //Student student = new Student(studentName2.getText().toString(), paid2.isChecked(), Integer.parseInt(sessionNumbers2.getText().toString()), id);
-               // myRef.child("Student"+studentId.getText()).setValue(student);
-                studentId.setText("");
-                studentName2.setText("");
-                paid2.setChecked(false);
-                sessionNumbers2.setText("");
-            }
-        });
 
 
 
 
     }
+
+    public void displayView(int viewId)
+    {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+
+        switch (viewId)
+        {
+            case R.id.menu_students:
+                fragment = new StudentsFragment();
+                title = "Students";
+                break;
+
+            case R.id.menu_agenda:
+                fragment = new AgendaFragment();
+                title = "Agenda";
+                break;
+
+            default:
+                break;
+
+        }
+
+        if (fragment!=null)
+        {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragmentPlaceholder, fragment);
+            ft.commit();
+        }
+
+    }
+
 
 
 
